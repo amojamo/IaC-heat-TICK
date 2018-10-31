6ba9aa96341cfd42646a8ec844aaf456d1ea3a46
Adrian <amojamo@amo.com>
2018-10-31 13:09:41 +0000

    Changed templates for our purpose

--- lib/manager_boot.bash
+++ lib/manager_boot.bash
@@ -1,13 +1,14 @@
 #!/bin/bash -v
 tempdeb=$(mktemp /tmp/debpackage.XXXXXXXXXXXXXXXXXX) || exit 1
-wget -O "$tempdeb" https://apt.puppetlabs.com/puppet5-release-bionic.deb
+wget -O "$tempdeb" https://apt.puppetlabs.com/puppet6-release-bionic.deb
 dpkg -i "$tempdeb"
 apt-get update
 apt-get -y install puppetserver pwgen
 /opt/puppetlabs/bin/puppet resource service puppet ensure=stopped enable=true
 /opt/puppetlabs/bin/puppet resource service puppetserver ensure=stopped enable=true
 # configure puppet agent, and puppetserver autosign
-/opt/puppetlabs/bin/puppet config set server manager.borg.trek --section main
+/opt/puppetlabs/bin/puppet config set server puppetmaster --section main
+/opt/puppetlabs/bin/puppet config set certname puppetmaster --section main
 /opt/puppetlabs/bin/puppet config set runinterval 300 --section main
 /opt/puppetlabs/bin/puppet config set autosign true --section master
 # keys for hiera-eyaml TBA...
@@ -17,7 +18,7 @@ cat <<EOF > /var/tmp/r10k.pp
 class { 'r10k':
   sources => {
     'puppet' => {
-      'remote'  => 'https://github.com/githubgossin/control-repo-k8s.git',
+      'remote'  => 'https://github.com/amojamo/imt3005tick.git',
       'basedir' => '/etc/puppetlabs/code/environments',
       'prefix'  => false,
     },
@@ -30,18 +31,10 @@ cd /etc/puppetlabs/code/environments/production/
 bash ./new_keys_and_passwds.bash
 # temp fix to have correct fqdn for puppet, before permanent fix in dhclient
 # (which requires reboot)
-echo "$(ip a | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | tr -d 'inet ' | grep -v '^127') $(hostname).borg.trek $(hostname)" >> /etc/hosts
+echo "$(ip a | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | tr -d 'inet ' | grep -v '^127') $(hostname)" >> /etc/hosts
 /opt/puppetlabs/bin/puppet resource service puppetserver ensure=running enable=true
 /opt/puppetlabs/bin/puppet agent -t # request certificate
 /opt/puppetlabs/bin/puppet agent -t # configure manager
 /opt/puppetlabs/bin/puppet agent -t # once more to update exported resources
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
-# permanent fix for domainname
-cat <<EOF >> /etc/netplan/50-cloud-init.yaml
-            nameservers:
-                search: [borg.trek]
-                addresses: [127.0.0.1]
-EOF
-netplan apply
 
-#wc_notify --data-binary '{"status": "SUCCESS"}'
