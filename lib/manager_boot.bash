ef9c79718219b039899d073855eb3513527402ae
Adrian <amojamo@amo.com>
2018-11-02 09:44:52 +0000

    Fixed git repo permissionsv2

--- lib/manager_boot.bash
+++ lib/manager_boot.bash
@@ -6,19 +6,20 @@ apt-get update
 apt-get -y install puppetserver pwgen
 /opt/puppetlabs/bin/puppet resource service puppet ensure=stopped enable=true
 /opt/puppetlabs/bin/puppet resource service puppetserver ensure=stopped enable=true
+
 # configure puppet agent, and puppetserver autosign
-/opt/puppetlabs/bin/puppet config set server puppetmaster --section main
-/opt/puppetlabs/bin/puppet config set certname puppetmaster --section main
+/opt/puppetlabs/bin/puppet config set server manager --section main
+/opt/puppetlabs/bin/puppet config set certname manager --section main
 /opt/puppetlabs/bin/puppet config set runinterval 300 --section main
 /opt/puppetlabs/bin/puppet config set autosign true --section master
-# keys for hiera-eyaml TBA...
+
 # r10 and control-repo:
 /opt/puppetlabs/bin/puppet module install puppet-r10k
 cat <<EOF > /var/tmp/r10k.pp
 class { 'r10k':
   sources => {
     'puppet' => {
-      'remote'  => 'https://github.com/amojamo/imt3005tick.git',
+      'remote'  => 'https://17e9e585a26ada8416acce546351c19d405ad7c0@github.com/amojamo/imt3005tick.git',
       'basedir' => '/etc/puppetlabs/code/environments',
       'prefix'  => false,
     },
@@ -27,14 +28,14 @@ class { 'r10k':
 EOF
 /opt/puppetlabs/bin/puppet apply /var/tmp/r10k.pp
 r10k deploy environment -pv
-cd /etc/puppetlabs/code/environments/production/
-bash ./new_keys_and_passwds.bash
-# temp fix to have correct fqdn for puppet, before permanent fix in dhclient
-# (which requires reboot)
+
+# Add hostname to hosts
 echo "$(ip a | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | tr -d 'inet ' | grep -v '^127') $(hostname)" >> /etc/hosts
+
 /opt/puppetlabs/bin/puppet resource service puppetserver ensure=running enable=true
 /opt/puppetlabs/bin/puppet agent -t # request certificate
 /opt/puppetlabs/bin/puppet agent -t # configure manager
 /opt/puppetlabs/bin/puppet agent -t # once more to update exported resources
 /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+
 
